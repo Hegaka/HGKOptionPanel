6b08ffcb4e027cc08ca5bba8519dd284c530662c
Jon Arrien <jonarrien@gmail.com>
2011-09-21 10:27:36 +0200

    ...

---
+++ HGKOptionPanel/HGKOptionPanel/HGKOptionPanelAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HGKOptionPanelAppDelegate.h
+//  HGKOptionPanel
+//
+//  Created by Jon Arrien  Fernández on 21/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGKOptionPanelAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
